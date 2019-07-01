dc5215722eda6315a78cd03fb076e97789aa81ea
biohuns <18520474+biohuns@users.noreply.github.com>
2019-07-01 10:51:19 +0900

    gitignore_global -> .config/git/ignore

--- script/mklink.sh
+++ script/mklink.sh
@@ -4,6 +4,8 @@ set -eu
 
 source $(dirname $0)/env.sh
 
+require fish
+
 for f in .??*
 do
     # ignore git
@@ -19,3 +21,18 @@ done
 
 # vim
 curl https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim -o ~/.vim/colors/darcula.vim
+
+# git
+[ ! -e $HOME/.config/git ] && mkdir -p $HOME/.config/git
+for f in .config/git/*
+do
+    create_symlink $f
+done
+
+# fish shell
+[ ! -e $HOME/.config/fish ] && mkdir -p $HOME/.config/fish
+for f in .config/fish/*
+do
+    create_symlink $f
+done
+[ "$SHELL" = "$(which fish)" ] || (chsh -s $(which fish); fish)
