03f8582a89d2c9e8a291558b8e52d2041c976261
biohuns <18520474+biohuns@users.noreply.github.com>
2019-08-16 16:20:48 +0900

    mklink.sh修正

--- script/mklink.sh
+++ script/mklink.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
-
 set -eu
 
-source $(dirname $0)/env.sh
+DIR="$(cd "$(dirname "$0")"/../ && pwd)"
+
+complete() {
+    echo "complete: $1"
+}
+
+require () {
+    for c in "$@"; do
+        type "$c" > /dev/null
+    done
+}
+
+create_symlink () {
+    ln -snfv "$DIR/$1" "$HOME"/"$1" > /dev/null
+    complete "$1"
+}
 
 require fish
 
 for f in .??*
 do
+    # ignore .example
+    [[ "$f" = *".example" ]] && continue
     # ignore git
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitignore" ] && continue
@@ -16,23 +32,26 @@ do
     # ignore .config
     [ "$f" = ".config" ] && continue
 
-    create_symlink $f
+    create_symlink "$f"
 done
 
 # vim
-curl https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim -o ~/.vim/colors/darcula.vim
+curl -s https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim -o ~/.vim/colors/darcula.vim
+complete ".vim/colors"
 
 # git
-[ ! -e $HOME/.config/git ] && mkdir -p $HOME/.config/git
+[ ! -e "$HOME/.config/git" ] && mkdir -p "$HOME/.config/git"
 for f in .config/git/*
 do
-    create_symlink $f
+    create_symlink "$f"
 done
 
 # fish shell
-[ ! -e $HOME/.config/fish ] && mkdir -p $HOME/.config/fish
+[ ! -e "$HOME/.config/fish" ] && mkdir -p "$HOME/.config/fish"
 for f in .config/fish/*
 do
-    create_symlink $f
+    create_symlink "$f"
 done
-[ "$SHELL" = "$(which fish)" ] || (chsh -s $(which fish); fish)
+[ "$SHELL" = "$(command -v fish)" ] || (chsh -s "$(command -v fish)"; fish)
+
+echo "Success!"
